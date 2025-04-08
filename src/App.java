9b4a7af23ab54772cb6f5ba7264458f858b6ce19
Simon Nestler <simon.nestler@thi.de>
2025-04-08 12:09:25 +0200

    drei referenzen. drei personen.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Person p1 = new Person("Max");
+        Person p2 = new Person("Emma");
+        Person p3 = new Person("Erna");
     }
 }
