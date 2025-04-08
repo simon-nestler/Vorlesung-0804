d95a936440d495da71ab5dddaf9bc06599388a4f
Simon Nestler <simon.nestler@thi.de>
2025-04-08 12:44:12 +0200

    max mit mutter :-)

--- src/App.java
+++ src/App.java
@@ -1,15 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Object[] personen = new Object[10];
-
-        personen[0] = new Person("Max");
-        personen[1] = new Person("Peter");
-        personen[2] = new Person("Anna");
-
-        for (int i = 0; i < personen.length; i++) {
-            if (personen[i] != null) {
-                System.out.println(personen[i]);
-            }
-        }
+        Person max = new Person("Max");
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,25 @@
 public class Person {
     private String name;
+    private Person mutter;
 
     public Person(String name) {
         this.name = name;
+        System.out.println(this);
+        new Person(new Person(new Person(new Person(new Person(this)))));
+    }
+
+    public Person(Person kind) {
+        this.name = "Mutter von " + kind.getName();
+        System.out.println(this);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String toString() {
         return "Person {" +
                 "name='" + name + '\'' +
                 '}';
-    }
-
-    public void sagHallo() {
-        System.out.println("Hallo, ich bin " + name);
     }
 }
