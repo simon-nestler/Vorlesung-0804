eb9b932cb723d72e1d5c9686152639864cceceed
Simon Nestler <simon.nestler@thi.de>
2025-04-08 12:48:29 +0200

    jedes kind kennt die eigene mutter

--- src/Person.java
+++ src/Person.java
@@ -5,16 +5,23 @@ public class Person {
     public Person(String name) {
         this.name = name;
         System.out.println(this);
-        new Person(new Person(new Person(new Person(new Person(this)))));
+
+        this.mutter = new Person(this);
+        new Person(new Person(new Person(new Person(this.mutter))));
     }
 
     public Person(Person kind) {
         this.name = "Mutter von " + kind.getName();
+        kind.setMutter(this);
         System.out.println(this);
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setMutter(Person mutter) {
+        this.mutter = mutter;
     }
 
     public String toString() {
