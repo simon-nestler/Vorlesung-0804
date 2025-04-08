7e2e46336ab1cea4dc8e6e68e11b3b449d089551
Simon Nestler <simon.nestler@thi.de>
2025-04-08 12:28:42 +0200

    speichern der personen in einem person array und durchlaufen des arrays mithilfe einer for-schleife

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Person p1 = new Person("Max");
-        Person p2 = new Person("Emma");
-        Person p3 = new Person("Erna");
+        Person[] personen = new Person[10];
+
+        personen[0] = new Person("Max");
+        personen[1] = new Person("Peter");
+        personen[2] = new Person("Anna");
+
+        for (int i = 0; i < personen.length; i++) {
+            if (personen[i] != null) {
+                personen[i].sagHallo();
+            }
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,14 @@ public class Person {
     public Person(String name) {
         this.name = name;
     }
+
+    public String toString() {
+        return "Person {" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public void sagHallo() {
+        System.out.println("Hallo, ich bin " + name);
+    }
 }
