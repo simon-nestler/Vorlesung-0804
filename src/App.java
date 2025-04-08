f800d9fa85690b3d3aa647161095959eb80b46da
Simon Nestler <simon.nestler@thi.de>
2025-04-08 13:00:06 +0200

    durchlaufen der liste in der main methode

--- src/App.java
+++ src/App.java
@@ -2,5 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         Person max = new Person("Max");
         max.getUrahnin().sagHallo();
+
+        System.out.println("------------------");
+
+        Person p = max;
+        do {
+            p.sagHallo();
+            p = p.getMutter();
+        } while (p != null);
+
     }
 }
